7ffab66650e2ceebb9db50609edf8b2c9b14372b
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-21 03:16:42 +0000

    variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234
+let accountPassword = 4321
+var accountEmail = "account@gmail.com"
+
+// const accountId = 5678
+// let accountPassword = 1234
+var accountEmail = "account@gmail2.com"
+
+console.table([accountEmail, accountId, accountPassword])
