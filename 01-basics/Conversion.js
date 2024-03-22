83f9781f5d92b91b44a2db5847e39245ce3f985a
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-22 02:56:29 +0000

    conversion

---
+++ 01-basics/Conversion.js
@@ -0,0 +1,24 @@
+// let number = 1;
+// console.log(number);
+// console.log(typeof number);
+// let booleanNumber = Bool(number);
+// console.log(booleanNumber);
+// console.log(typeof number);
+
+let num = 33
+console.log(typeof num);
+console.log(num);
+
+let stringNum = String(num);
+console.log(typeof stringNum);
+console.log(stringNum);
+
+let turu = "1";
+let boolTuru = Boolean(turu);
+console.log(typeof boolTuru);
+console.log(boolTuru);
+
+// 0 => false 
+// 1 => true
+// "" = false
+// "Gautam" => true
