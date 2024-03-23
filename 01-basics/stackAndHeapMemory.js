b36c56c70d281aa6856ce19c1517ade20781d11c
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-23 08:04:07 +0000

    memory management

---
+++ 01-basics/stackAndHeapMemory.js
@@ -0,0 +1,16 @@
+let id1 = "0827CS221095"
+let id2 = id1
+id2 = "0897CS231076" 
+console.log(id1);
+console.log(id2);
+
+
+
+let first = {
+    name: "Rahul",
+    email: "rahul@gmail.com"
+}
+let second = first
+first.name = "Naman"
+console.log(first.name);
+console.log(second.name);
