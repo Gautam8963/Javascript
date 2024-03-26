34e8f4b8d58dc5222508da1bde491341e8b6f44f
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-26 15:16:01 +0000

    Date and Time

---
+++ 01-basics/date.js
@@ -0,0 +1,8 @@
+// let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+const myDob = new Date("2005-02-02")
+console.log(myDob.toLocaleString());
