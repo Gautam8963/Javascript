82eb22976096d239d57794870f154c3d2240406e
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-26 14:38:26 +0000

    Numbers and Maths

---
+++ 01-basics/num-math.js
@@ -0,0 +1,14 @@
+// console.log("Gautam Dhodi")
+// const score = new Number(87.89)
+// console.log(score.toPrecision(3) );
+
+const money = 10000000
+console.log(money.toLocaleString('en-In'));
+
+
+// ***************************MATHS*******************************************
+console.log(Math.round(4.7));
+console.log(Math.random())
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() + (max - min + 1)) + min);
