93a9c0de93b33d5171fccfd2f1dc089045b52080
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-26 16:13:02 +0000

    Array in js

---
+++ 02-basics/array.js
@@ -0,0 +1,18 @@
+const arr = [1,2,3,4,5]
+// console.log(arr);
+// arr.push(6)
+// arr.push(7)
+// arr.push(8)
+// console.log(arr);
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// console.log(arr);
+// arr.unshift(0)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+console.log(arr.slice(1,3));
+console.log(arr)
+console.log(arr.splice(1,3));
+console.log(arr)
