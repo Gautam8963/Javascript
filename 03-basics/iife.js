603d0ff92c0e3565618b1cae67c79956289a3ded
Gautam8963 <111161793+Gautam8963@users.noreply.github.com>
2024-03-29 02:21:00 +0000

    iife function

---
+++ 03-basics/iife.js
@@ -0,0 +1,8 @@
+//   ********* IIFE FUNCTION *********
+
+(function chai(){
+    console.log("welcome to chai and code");
+})();
+
+(  (name) => {console.log('Welcome to hiteshchoudhary by ${name}');})('Hitesh Choudhary')
+
